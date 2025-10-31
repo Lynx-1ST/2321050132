fbb78c6b99e68a6df832bb3b59d15b312f5a397d
Lynx_1ST <maihang122th@gmail.com>
2025-10-31 11:30:30 +0700

    update 31/10

---
+++ Oct31_B13/index.js
@@ -0,0 +1,21 @@
+/* let cheDo = "sang";
+function readMode() {
+  if (cheDo) {
+    document.getElementsByTagName("body")[0].style = "background-color: black;";
+    document.getElementsByTagName("img")[0].src =
+      "../../25K1_WebDesign/assets/moon.jpg";
+    document.getElementsByTagName("h1")[1].innerHTML = "Chế độ tối";
+    document.getElementsByTagName("h1")[0].style = "color: white";
+    document.getElementsByTagName("h1")[1].style = "color: white";
+    cheDo = false;
+  } else {
+    document.getElementsByTagName("body")[0].style = "background-color: white;";
+    document.getElementsByTagName("img")[0].src =
+      "../../25K1_WebDesign/assets/sun.jpg";
+    document.getElementsByTagName("h1")[1].innerHTML = "Chế độ sáng";
+    document.getElementsByTagName("h1")[0].style = "color: black";
+    document.getElementsByTagName("h1")[1].style = "color: black";
+    cheDo = true;
+  }
+}
+ */
